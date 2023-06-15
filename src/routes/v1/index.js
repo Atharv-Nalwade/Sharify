124687f6dc9f257eefde735eb035e187b548c7d8
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-06-15 12:40:00 +0530

    refactor:Modified code to use Jszip library for compression instead of zlib

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const UploadController = require('../../controllers/upload-controller.js');
 const DownloadController = require('../../controllers/download-controller.js');
-const zlib = require('zlib');
+const JSZip = require('jszip');
+const fs = require('fs');
 const { s3 } = require('../../config/awsConfig.js');
 
 const router = express.Router();
@@ -11,51 +12,46 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 const customUploadMiddleware = async (req, res, next) => {
-    try {
-      upload.single('file')(req, res, async function (err) {
-        if (err) {
-          return next(err);
-        }
-        console.log("Original",req.file.buffer.length)
-        // File upload successful, now compress the uploaded file
-        try {
-          const fileBuffer = req.file.buffer;
-          const compressionLevel = zlib.constants.Z_BEST_COMPRESSION;
+  try {
+    upload.single('file')(req, res, async function (err) {
+      if (err) {
+        return next(err);
+      }
+      console.log("Original", req.file.buffer.length);
 
-        const compressedBuffer = zlib.gzipSync(fileBuffer, { level: compressionLevel });
-          console.log("Compressed",compressedBuffer.length)
-  
-          const filePath = `files/${req.file.originalname}.gz`; // Include the .gz extension in the file path
-  
-          const uploadParams = {
-            Bucket: process.env.Bucket,
-            Key: filePath,
-            Body: compressedBuffer,
-            ContentEncoding: 'gzip',
-            ContentType: req.file.mimetype,
-          };
-  
-          const s3Upload = s3.upload(uploadParams).promise();
-          const s3Data = await s3Upload;
-  
-          req.file.location = s3Data.Location; // Update the file location with the S3 file path
-          req.file.originalname = req.file.originalname + '.gz'; // Update the original name with the .gz extension
-  
-          next();
-        } catch (err) {
-          next(err);
-        }
-      });
-    } catch (err) {
-      next(err);
-    }
-  };
+      try {
+        const fileBuffer = req.file.buffer;
 
+        const zip = new JSZip();
+        zip.file(req.file.originalname, fileBuffer);
 
-  
-  
-  
-  
+        const compressedBuffer = await zip.generateAsync({ type: 'nodebuffer' });
+        console.log("Compressed", compressedBuffer.length);
+
+        const filePath = `files/${req.file.originalname}.zip`;
+
+        const uploadParams = {
+          Bucket: process.env.Bucket,
+          Key: filePath,
+          Body: compressedBuffer,
+          ContentType: 'application/zip',
+        };
+
+        const s3Upload = s3.upload(uploadParams).promise();
+        const s3Data = await s3Upload;
+
+        req.file.location = s3Data.Location;
+        req.file.originalname = req.file.originalname + '.zip';
+
+        next();
+      } catch (err) {
+        next(err);
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 router.post('/upload', customUploadMiddleware, UploadController.uploadController);
 router.get('/download/:id', DownloadController.download);
